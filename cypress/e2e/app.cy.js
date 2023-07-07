ee8748beaa726586e48d87a3770a74a062c6f9c6
fuegoai <fuego@incube8.sg>
2023-07-07 11:23:21 +0800

    update name

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,5 +1,5 @@
 describe("Navigation", () => {
-  it("should navigate to the about page", () => {
+  it("should navigate to the index page", () => {
     // Start from the index page
     cy.visit("http://localhost:3000/");
 
