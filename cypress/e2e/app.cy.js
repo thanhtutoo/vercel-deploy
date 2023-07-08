ac72e763916b03962fe251637740b714bea555f3
fuegoai <fuego@incube8.sg>
2023-07-08 15:27:14 +0800

    update cart

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -2,8 +2,6 @@ describe("Navigation", () => {
   it("should navigate to the index page", () => {
     // Start from the index page
     cy.visit("http://localhost:3000/");
-
-    // Find a link with an href attribute containing "about" and click it
     cy.get('[data-cy-filter="min-price"]').should("exist");
   });
 });
